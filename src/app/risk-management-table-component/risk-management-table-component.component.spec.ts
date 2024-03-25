8d14cee540570d1e1914da17c9d44f5cb9033813
naushik007 <naushik007@gmail.com>
2024-03-25 02:29:19 -0700

    Update risk-management-table-component.component.spec.ts

--- src/app/risk-management-table-component/risk-management-table-component.component.spec.ts
+++ src/app/risk-management-table-component/risk-management-table-component.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RiskManagementTableComponentComponent } from './risk-management-table-component.component';
+import { RiskManagementTableComponent } from './risk-management-table-component.component';
 
-describe('RiskManagementTableComponentComponent', () => {
-  let component: RiskManagementTableComponentComponent;
-  let fixture: ComponentFixture<RiskManagementTableComponentComponent>;
+describe('RiskManagementTableComponent', () => {
+  let component: RiskManagementTableComponent;
+  let fixture: ComponentFixture<RiskManagementTableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RiskManagementTableComponentComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(RiskManagementTableComponentComponent);
+      imports: [RiskManagementTableComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RiskManagementTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
