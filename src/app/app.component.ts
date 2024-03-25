da374a6f7e6f602587634726945b0672d48c3a29
naushik007 <naushik007@gmail.com>
2024-03-25 03:54:02 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Hello, risk-management-plugin';
+  title = 'risk-management-plugin';
 }
